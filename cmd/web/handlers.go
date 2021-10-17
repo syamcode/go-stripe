062e9f77212109ea6ff88c23774f283fa3df0019
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-10-17 14:57:50 +0700

    Show transaction receipt

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,3 +7,33 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 		app.errorLog.Println(err)
 	}
 }
+
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["payment_intent"] = paymentIntent
+	data["payment_method"] = paymentMethod
+	data["payment_amount"] = paymentAmount
+	data["payment_currency"] = paymentCurrency
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
