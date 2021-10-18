90f4a4be0e20c3f4220e53ce2dff2d03ef69be84
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-10-18 12:39:37 +0700

    Create widget model

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/syamcode/go-stripe/internal/models"
+)
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
@@ -46,8 +50,21 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["publishable_key"] = app.config.stripe.key
+
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
 	if err := app.renderTemplate(w, r, "buy-once", &templateData{
 		StringMap: stringMap,
+		Data:      data,
 	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
