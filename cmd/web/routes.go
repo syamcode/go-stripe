5aae6e65d4e2b5fd36d6f07039ac126d24cf8584
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-10-23 12:44:24 +0700

    Fix virtual terminal page

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,9 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 	mux.Get("/virtual-terminal", app.VirtualTerminal)
+	mux.Post("/virtual-terminal-payment-succeeded", app.VirtualTerminalPaymentSucceeded)
 	mux.Post("/payment-succeeded", app.PaymentSucceeded)
+	mux.Get("/virtual-terminal-receipt", app.VirtualTerminalReceipt)
 	mux.Get("/receipt", app.Receipt)
 
 	mux.Get("/widget/{id}", app.ChargeOnce)
