e516afdbdf92d44a434aa1113ffe24b75c72189b
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-10-25 08:40:48 +0700

    Create login page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -281,3 +281,10 @@ func (app *application) BronzePlanReceipt(w http.ResponseWriter, r *http.Request
 		app.errorLog.Println(err)
 	}
 }
+
+//LoginPage displays the login page
+func (app *application) LoginPage(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "login", &templateData{}); err != nil {
+		app.errorLog.Println(err)
+	}
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -21,6 +21,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/plans/bronze", app.BronzePlan)
 	mux.Get("/receipt/bronze", app.BronzePlanReceipt)
 
+	mux.Get("/login", app.LoginPage)
+
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
