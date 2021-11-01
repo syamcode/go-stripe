c64c646971839d3c291748685d2cd76f879e1b30
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-11-02 01:44:37 +0700

    Create backend route to handle passwod resets

--- internal/models/models.go
+++ internal/models/models.go
@@ -296,3 +296,15 @@ func (m *DBModel) Authenticate(email, password string) (int, error) {
 
 	return id, nil
 }
+
+func (m *DBModel) UpdatePasswordForUser(u User, password string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, "update users set password = ? where id = ?", password, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
