a15be7901783dd80409192485210169190458a89
Muhamad Hisyam Anshory <syamcode@gmail.com>
2021-11-06 17:21:59 +0700

    Receive data in the microservice

--- cmd/micro/invoice/invoice-handlers.go
+++ cmd/micro/invoice/invoice-handlers.go
@@ -3,8 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
+type Order struct {
+	ID        int       `json:"id"`
+	Quantity  int       `json:"quantity"`
+	Amount    int       `json:"amount"`
+	Product   string    `json"product"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func (app *application) CreateAndSendInvoice(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	// receive json
+	var order Order
+	err := app.readJSON(w, r, &order)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	// generate a pdf invoice
+
+	// create mail
+
+	// send mail with attachment
+
+	// send response
+	var resp struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+	resp.Error = false
+	resp.Message = fmt.Sprintf("Invoice %d.pdf created and sent to %s", order.ID, order.Email)
+	app.writeJSON(w, http.StatusCreated, resp)
 }
